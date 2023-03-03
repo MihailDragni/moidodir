6d773c4353d7b6f748fa1869e3fc88876c4c8126
Mihail Dragni <dragni95@mail.ru>
2023-03-03 18:16:14 +0600

    added catalog

--- src/js/files/category.js
+++ src/js/files/category.js
@@ -1,64 +1,106 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   //UI
-//   const burgerMenu = document.querySelector(".catalog");
-//   const burgerBtn = document.querySelector(".catalog-btn");
-//   const dropMenu = document.querySelector(".catalog-drop");
-//   const burgerCloseBtn = document.querySelector(".catalog__close-btn");
-//   const dropMenuList = document.querySelectorAll(".catalog-drop__item");
-//   const body = document.body;
+document.addEventListener("DOMContentLoaded", () => {
+  //UI
+  const body = document.body;
+  const catalog = document.querySelector(".catalog");
+  const catalogBtn = document.querySelector(".catalog-btn");
+  const catalogCloseBtn = document.querySelector(".catalog__close-btn");
+  const secondColumn = document.querySelector(".catalog__second-column");
+  const secondColumnList = document.querySelectorAll(
+    ".catalog__second-column .column-catalog__item"
+  );
+  const thirdColumn = document.querySelector(".catalog__third-column");
+  const thirdColumnList = document.querySelectorAll(
+    ".catalog__third-column .column-catalog__item"
+  );
 
-//   //Events
-//   burgerBtn.addEventListener("click", openBurgerMenu);
-//   burgerMenu.addEventListener("mouseover", openDropMenu);
-//   burgerCloseBtn.addEventListener("click", closeBtnHeadler);
+  //Events
+  catalogBtn.addEventListener("click", openCatalog);
+  catalog.addEventListener("mouseover", openSecondColumn);
+  catalogCloseBtn.addEventListener("click", closeBtnHeadler);
+  secondColumn.addEventListener("click", openThirdColumn);
 
-//   function openBurgerMenu(event) {
-//     event.stopPropagation();
-//     if (
-//       event.target.classList.contains("catalog-btn") ||
-//       event.target.closest(".catalog-btn")
-//     ) {
-//       burgerMenu.classList.add("catalog--active");
-//       body.classList.add("lock");
-//       body.addEventListener("click", removeBurgerMenu);
-//     }
-//   }
+  function openCatalog(event) {
+    event.stopPropagation();
+    if (
+      event.target.classList.contains("catalog-btn") ||
+      event.target.closest(".catalog-btn")
+    ) {
+      catalog.classList.add("catalog--active");
+      body.classList.add("body-lock");
+      body.addEventListener("click", removeCatalog);
+    }
+  }
 
-//   function removeBurgerMenu({ target }) {
-//     if (target.classList.contains("catalog") || target.closest(".catalog")) {
-//       return;
-//     }
-//     closeMenu();
-//   }
+  function removeCatalog({ target }) {
+    if (target.classList.contains("catalog") || target.closest(".catalog")) {
+      return;
+    }
+    closeMenu();
+  }
 
-//   function openDropMenu(event) {
-//     if (event.target.classList.contains("burger-main-list__link")) {
-//       dropMenu.classList.add("catalog-drop--active");
-//       const elemId = event.target.dataset.category;
-//       let currentElem = null;
-//       [...dropMenuList].forEach((elem) => {
-//         elem.classList.remove("catalog-drop__item--active");
-//         if (elem.id === elemId) {
-//           elem.classList.add("catalog-drop__item--active");
-//           currentElem = elem;
-//         }
-//       });
-//     }
-//   }
+  function openSecondColumn({ target }) {
+    if (target.classList.contains("main-column-catalog__link")) {
+      thirdColumn.classList.remove("_active");
+      secondColumn.classList.add("_active");
+      const elemId = target.dataset.mainCategory;
+      [...secondColumnList].forEach((elem) => {
+        elem.classList.remove("column-catalog__item--active");
+        if (elem.id === elemId) {
+          elem.classList.add("column-catalog__item--active");
+        }
+      });
+    }
+  }
 
-//   function closeBtnHeadler({ target }) {
-//     if (
-//       target.classList.contains("catalog__close-btn") ||
-//       target.closest(".catalog__close-btn")
-//     ) {
-//       closeMenu();
-//     }
-//   }
+  function openThirdColumn(event) {
+    if (
+      event.target.closest(".drop-menu-list__link") &&
+      event.target.dataset.secondCategory
+    ) {
+      event.preventDefault();
+      const allSecondColumnLinks = document.querySelectorAll(
+        ".catalog__second-column .drop-menu-list__link"
+      );
+      let clickedElem = event.target.closest(".drop-menu-list__link");
+      if (clickedElem.classList.contains("_active")) {
+        allSecondColumnLinks.forEach((elem) =>
+          elem.classList.remove("_active")
+        );
+        thirdColumn.classList.remove("_active");
+        return;
+      }
+      allSecondColumnLinks.forEach((elem) => elem.classList.remove("_active"));
+      clickedElem.classList.add("_active");
+      thirdColumn.classList.add("_active");
+      const elemId = event.target.dataset.secondCategory;
+      [...thirdColumnList].forEach((elem) => {
+        elem.classList.remove("column-catalog__item--active");
+        if (elem.id === elemId) {
+          elem.classList.add("column-catalog__item--active");
+        }
+      });
+    }
+  }
 
-//   function closeMenu() {
-//     burgerMenu.classList.remove("catalog--active");
-//     dropMenu.classList.remove("catalog-drop--active");
-//     body.classList.remove("lock");
-//     body.removeEventListener("click", removeBurgerMenu);
-//   }
-// });
+  function closeBtnHeadler({ target }) {
+    if (
+      target.classList.contains("catalog__close-btn") ||
+      target.closest(".catalog__close-btn")
+    ) {
+      closeMenu();
+    }
+  }
+
+  function closeMenu() {
+    catalog.classList.remove("catalog--active");
+    secondColumn.classList.remove("column-catalog--active");
+    [...secondColumnList].forEach((elem) => {
+      elem.classList.remove("column-catalog__item--active");
+    });
+    [...thirdColumnList].forEach((elem) => {
+      elem.classList.remove("column-catalog__item--active");
+    });
+    body.classList.remove("body-lock");
+    body.removeEventListener("click", removeCatalog);
+  }
+});
