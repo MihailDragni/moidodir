5c30476291806a87366a808e9b2f59bc6c6037f9
Mihail Dragni <dragni95@mail.ru>
2023-03-12 03:37:26 +0600

    added catalog page

--- src/js/files/select2.js
+++ src/js/files/select2.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-  $(".js-example-basic-single").select2();
+  $(".js-example-basic-single").select2({
+    width: "auto",
+    minimumResultsForSearch: 10,
+  });
+  $(".filters-catalog__select").select2({
+    // multiple: true,
+    width: "auto",
+    selectionCssClass: "filters-catalog__title",
+    dropdownCssClass: "filters-catalog__dropdown",
+  });
 });
