a4e8f664b3e7c0d62f9de9f541511bbf5e344960
Mihail Dragni <dragni95@mail.ru>
2023-03-15 22:17:02 +0600

    added register page

--- src/js/files/forms/datepicker.js
+++ src/js/files/forms/datepicker.js
@@ -5,22 +5,33 @@
 import { flsModules } from "../modules.js";
 
 // Підключення модуля
-import datepicker from 'js-datepicker';
+import datepicker from "js-datepicker";
 
-if (document.querySelector('[data-datepicker]')) {
-	const picker = datepicker('[data-datepicker]', {
-		customDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Нд"],
-		customMonths: ["Січ", "Лют", "Берез", "Квіт", "Трав", "Черв", "Лип", "Серп", "Верес", "Жовт", "Листоп", "Груд"],
-		overlayButton: 'Застосувати',
-		overlayPlaceholder: 'Рік (4 цифри)',
-		startDay: 1,
-		formatter: (input, date, instance) => {
-			const value = date.toLocaleDateString()
-			input.value = value
-		},
-		onSelect: function (input, instance, date) {
-
-		}
-	});
-	flsModules.datepicker = picker;
+if (document.querySelector("[data-datepicker]")) {
+  const picker = datepicker("[data-datepicker]", {
+    customDays: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+    customMonths: [
+      "Январь",
+      "Февраль",
+      "Март",
+      "Апрель",
+      "Май",
+      "Июнь",
+      "Июль",
+      "Август",
+      "Сентябрь",
+      "Октябрь",
+      "Ноябрь",
+      "Декабрь",
+    ],
+    overlayButton: "Применить",
+    overlayPlaceholder: "Год (4 цифры)",
+    startDay: 1,
+    formatter: (input, date, instance) => {
+      const value = date.toLocaleDateString();
+      input.value = value;
+    },
+    onSelect: function (input, instance, date) {},
+  });
+  flsModules.datepicker = picker;
 }
